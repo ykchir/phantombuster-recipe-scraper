5641e698ce559b26ad3ba4f85309738df55a9c63
ykchir <yecine.kchir@gmail.com>
2024-12-24 17:25:53 +0100

    add warn logger method

--- src/shared/Logger.ts
+++ src/shared/Logger.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 export const Logger = {
   info: (message: string) => console.log(`[INFO] ${message}`),
+  warn: (message: string) => console.warn(`[WARN] ${message}`),
   error: (message: string, error?: Error) => {
     console.error(`[ERROR] ${message}`);
     if (error) console.error(error.stack);
